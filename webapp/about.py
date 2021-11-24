14cc09411e74b05af8139696d243c0078a6744ce
marshall <marshall.judas03@gmail.com>
2021-11-24 13:37:12 -0500

    Create home and about pages

---
+++ webapp/about.py
@@ -0,0 +1,20 @@
+import justpy as jp
+
+class About:
+    path = "/about"
+
+    def serve(self):
+        wp = jp.QuasarPage(tailwind=True)
+        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+        jp.Div(a=div, text="This is the About page!", classes="text-4xl m-2")
+        jp.Div(a=div, text="""
+        Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna 
+        aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut 
+        aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+        nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit 
+        anim id est laborum. 
+        """, classes="text-lg")
+        return wp
+
+
+
