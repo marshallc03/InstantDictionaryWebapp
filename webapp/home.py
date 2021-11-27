82203a08a595a6e1dc923bc24d4419bfce4272d5
marshall <marshall.judas03@gmail.com>
2021-11-27 15:51:50 -0500

    Made Routing automatic

--- webapp/home.py
+++ webapp/home.py
@@ -1,32 +1,18 @@
 import justpy as jp
+import layout
+import page
 
-class Home:
+
+class Home(page.Page):
     path = "/"
 
     @classmethod
     def serve(cls, req):
         wp = jp.QuasarPage(tailwind=True)
 
-        layout = jp.QLayout(a=wp, view="hHh lpR fFf")
-        header = jp.QHeader(a=layout)
-        toolbar = jp.QToolbar(a=header)
-        drawer = jp.QDrawer(a=layout, show_if_above=True, v_model="leftDrawerOpen", side="left", bordered=True)
+        lay = layout.DefaultLayout(a=wp)
 
-        scroller = jp.QScrollArea(a=drawer, classes="fit")
-        qlist = jp.QList(a=scroller)
-        a_classes = "p-2 m-2 text-lg text-blue-400 hover:text-blue-700"
-        jp.A(a=qlist, text="Home", href="/", classes=a_classes)
-        jp.Br(a=qlist)
-        jp.A(a=qlist, text="Dictionary", href="/dictionary", classes=a_classes)
-        jp.Br(a=qlist)
-        jp.A(a=qlist, text="About", href="/about", classes=a_classes)
-        jp.Br(a=qlist)
-
-        jp.QBtn(a=toolbar, dense=True, flat=True, round=True, icon="menu",
-                click=cls.move_drawer, drawer=drawer)
-        jp.QToolbarTitle(a=toolbar, text="Instant Dictionary")
-
-        container = jp.QPageContainer(a=layout)
+        container = jp.QPageContainer(a=lay)
 
         div = jp.Div(a=container, classes="bg-gray-200 h-screen")
         jp.Div(a=div, text="This is the Home page!", classes="text-4xl m-2")
@@ -38,10 +24,3 @@ class Home:
                 anim id est laborum. 
                 """, classes="text-lg")
         return wp
-
-    @staticmethod
-    def move_drawer(widget, msg):
-        if widget.drawer.value:
-            widget.drawer.value = False
-        else:
-            widget.drawer.value = True
